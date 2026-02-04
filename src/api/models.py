d0a7f6e518b07d62fa84d6b4d80ed8e20fb40ecf
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2026-02-04 17:15:34 +0000

    init

--- src/api/models.py
+++ src/api/models.py
@@ -1,15 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, select
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
@@ -17,3 +18,21 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    def register(self, email, password):
+        self.email = email
+        self.password = password
+        self.is_active = True
+
+        db.session.add(self)
+        db.session.commit()
+
+        return self.serialize()
+
+    def search_user(self, email, password):
+        find_user = db.session.execute(select(User).where(
+            User.email == email, User.password == password)).scalar_one_or_none()
+
+        if find_user is None:
+            return None
+        return find_user.serialize()
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,11 +5,15 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+user = User()
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -20,3 +24,41 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/register', methods=['POST'])
+def register():
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+
+    if email is None or password is None:
+        return jsonify({'msg': 'bad request'}), 400
+
+    created_user = user.register(email, password)
+
+    return jsonify(created_user), 201
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+
+    find_user = user.search_user(email, password)
+
+    if find_user is None:
+        return jsonify({'msg': 'auth err'}), 401
+
+    access_token = create_access_token(identity=str(find_user['id']))
+
+    return jsonify({"token": access_token})
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Accede a la identidad del usuario actual con get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(int(current_user_id))
+
+    return jsonify({"id": user.id, "email": user.email}), 200
